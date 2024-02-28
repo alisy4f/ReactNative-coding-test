c2df8eabb1e29b637873e4bb973cac49f0cda2b3
Aghil Robbani <aghil.robbani@codr.id>
2024-02-28 15:02:57 +0700

    implementing data storage

--- Splashscreen.js
+++ Splashscreen.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export default class Splashscreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+const SplashScreen = ({navigation, route}) => {
+
+  const checkDataUser = async () => {
+    const user = await AsyncStorage.getItem('dataUser')
+    console.log('cek data user', user); //data mentah
+    console.log('cek data user obj', JSON.parse(user)); //data sudah dirubah jadi object dan bisa digunakan
+    if (user) {
+      navigation.reset({ index: 0, routes: [{ name: 'Book' }] });
+    } else {
+      navigation.reset({ index: 0, routes: [{ name: 'Login' }] });
+    }
   }
 
-  render() {
-    return (
-      <View>
-        <Text> Splashscreen </Text>
-      </View>
-    );
-  }
+  useEffect(() => {    
+    setTimeout(() => {
+      checkDataUser();
+    }, 1500);
+  },[])
+
+  return (
+    <View>
+      <Text> Splashscreen </Text>
+    </View>
+  );
 }
+
+export default SplashScreen
